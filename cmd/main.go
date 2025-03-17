5f41db758ae653ac993a5dd262bd4ddb1f389dff
Kasjan Chilarski <keistzen@gmail.com>
2025-03-17 22:16:26 +0100

    add api configweaver scaffolding

--- cmd/main.go
+++ cmd/main.go
@@ -34,6 +34,9 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	weaverv1alpha1 "github.com/k-stz/config-weaver-operator/api/v1alpha1"
+	"github.com/k-stz/config-weaver-operator/internal/controller"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -43,8 +46,11 @@ var (
 )
 
 func init() {
+	// adds built-in Kinds to scheme
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	// Adds our type to scheme
+	utilruntime.Must(weaverv1alpha1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -140,6 +146,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controller.ConfigMapSyncReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ConfigMapSync")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
